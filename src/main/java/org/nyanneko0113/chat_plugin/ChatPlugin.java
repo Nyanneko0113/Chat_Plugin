f66a4c5b75e6f770ce185616dadf78c69bb0e413
Nyanneko0113 <yuuki01130201@gmail.com>
2024-08-05 19:45:02 +0900

    first commit

---
+++ src/main/java/org/nyanneko0113/chat_plugin/ChatPlugin.java
@@ -0,0 +1,17 @@
+package org.nyanneko0113.chat_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
